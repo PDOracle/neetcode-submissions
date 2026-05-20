5f925adb900535e561039bbbf9407ffc1f60e513
Matthew Ange Mao <114012137+PDOracle@users.noreply.github.com>
2026-05-20 13:38:55 -0400

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        alnum = ''.join(char.lower() for char in s if char.isalnum())
+        l, r = 0, len(alnum) - 1
+        while l <= r:
+            if alnum[l] != alnum[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
