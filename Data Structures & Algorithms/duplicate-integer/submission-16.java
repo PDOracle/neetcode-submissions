510cde952cb5a2a3340fec0255c137d52743d4d7
Matthew Ange Mao <114012137+PDOracle@users.noreply.github.com>
2026-05-27 23:51:55 -0400

    Add: duplicate-integer - submission-16

---
+++ Data Structures & Algorithms/duplicate-integer/submission-16.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> uniques = new HashSet<Integer>();
+        for(int num: nums){
+            if(uniques.contains(num)){
+                return true;
+            } else{
+                uniques.add(num);
+            }
+        }
+        return false;
+    }
+}
