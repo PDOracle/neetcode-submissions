48850de13c1170326a9329625a507b933deefff9
Matthew Ange Mao <114012137+PDOracle@users.noreply.github.com>
2026-05-28 00:01:00 -0400

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String lowerS = s.toLowerCase();
+        lowerS = lowerS.replaceAll(
+          "[^a-zA-Z0-9]", "");
+        int l = 0, r = lowerS.length() - 1;
+        while(l < r){
+            System.out.println("L: " + lowerS.charAt(l) + " R: " + lowerS.charAt(r));
+            if(lowerS.charAt(l) != lowerS.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
