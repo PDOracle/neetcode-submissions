7207d92343edd1a34bf3d73d83305cf66694eeff
Matthew Ange Mao <114012137+PDOracle@users.noreply.github.com>
2026-06-10 18:08:43 -0400

    Add: search-for-word - submission-16

---
+++ Data Structures & Algorithms/search-for-word/submission-16.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                if(word.charAt(0) == board[i][j]){
+                    int[][] state = new int[board.length][board[0].length];
+
+                    boolean found = sol(board, word, "", state, j, i);
+                    if(found){
+                        return true;
+                    }
+                }
+
+            }
+        }
+        return false;
+    }
+
+    public boolean sol(char[][] board, String word, String curr, int[][] state, int x, int y){
+        if(x > board[0].length - 1 || y > board.length - 1 || x < 0 || y < 0){
+            return false;
+        }
+        if(word.indexOf(curr) != 0){
+            return false;
+        }
+        if(state[y][x] == 1){
+            return false;
+        }
+        String newCurr = curr + board[y][x];
+        state[y][x] = 1;
+        if(word.equals(newCurr)){
+            return true;
+        }
+        boolean left = sol(board, word, newCurr, state, x - 1, y);
+        boolean right = sol(board, word, newCurr, state, x + 1, y);
+        boolean up = sol(board, word, newCurr, state, x, y - 1);
+        boolean down = sol(board, word, newCurr, state, x, y + 1);
+        state[y][x] = 0;
+        return left || right || up || down;
+    }
+}
