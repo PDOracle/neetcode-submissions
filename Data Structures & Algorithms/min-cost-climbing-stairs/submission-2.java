fd5fc4f943260dfa4136677a58c080e74b683940
Matthew Ange Mao <114012137+PDOracle@users.noreply.github.com>
2026-06-30 16:16:44 -0400

    Add: min-cost-climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int[] step_min = new int[cost.length];
+        step_min[0] = cost[0];
+        step_min[1] = cost[1];
+        return dp(cost.length, step_min, cost);
+
+    }
+
+    public int dp(int n, int[] step_cost, int[] cost){
+        if(n < 0){
+            return 0;
+        }
+        if(n <= 1){
+            return step_cost[n];
+        }
+        if(step_cost[n-2] == 0){
+            step_cost[n - 2] = cost[n-2] + dp(n - 2, step_cost, cost);
+        }
+        if(step_cost[n-1] == 0){
+            step_cost[n - 1] = cost[n-1] + dp(n - 1, step_cost, cost);
+        }
+        return Math.min(step_cost[n - 1], step_cost[n - 2]);
+    }
+}
