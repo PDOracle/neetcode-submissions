db577dae56230af97299320e7cd49ef0fada99f1
Matthew Ange Mao <114012137+PDOracle@users.noreply.github.com>
2026-07-04 20:43:13 -0400

    Add: implement-queue-using-stacks - submission-3

---
+++ Data Structures & Algorithms/implement-queue-using-stacks/submission-3.java
@@ -0,0 +1,54 @@
+class MyQueue {
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+    
+    public void push(int x) {
+        if(stack1.isEmpty() && stack2.isEmpty()){
+            stack1.add(x);
+        }
+        else{
+            while(!stack1.isEmpty()){
+                stack2.add(stack1.pop());
+            }
+            stack1.add(x);
+            while(!stack2.isEmpty()){
+                stack1.add(stack2.pop());
+            }
+        }
+    }
+    
+    public int pop() {
+        if(stack1.isEmpty()){
+            return stack2.pop();
+        }
+        else{
+            return stack1.pop();
+        }
+    }
+    
+    public int peek() {
+        if(stack1.isEmpty()){
+            return stack2.peek();
+        }
+        else{
+            return stack1.peek();
+        }
+    }
+    
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
