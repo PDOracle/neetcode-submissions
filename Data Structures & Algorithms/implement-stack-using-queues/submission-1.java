dfebcfd648edd8e2684319e4eee2eccbf799a242
Matthew Ange Mao <114012137+PDOracle@users.noreply.github.com>
2026-07-04 20:09:01 -0400

    Add: implement-stack-using-queues - submission-1

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-1.java
@@ -0,0 +1,56 @@
+class MyStack {
+    Queue<Integer> queue1;
+    Queue<Integer> queue2;
+
+    public MyStack() {
+        queue1 = new LinkedList<>();
+        queue2 = new LinkedList<>();    
+    }
+    
+    public void push(int x) {
+        if(queue1.isEmpty() && queue2.isEmpty()){
+            queue1.add(x);
+        }
+        else if(queue1.isEmpty()){
+            queue1.add(x);
+            while(!queue2.isEmpty()){
+                queue1.add(queue2.remove());
+            }
+        }
+        else{
+            queue2.add(x);
+            while(!queue1.isEmpty()){
+                queue2.add(queue1.remove());
+            }
+        }
+    }
+    
+    public int pop() {
+        if(queue1.isEmpty()){
+            return queue2.remove();
+        }
+        else{
+            return queue1.remove();
+        }
+    }
+    
+    public int top() {
+        if(queue1.isEmpty()){
+            return queue2.peek();
+        }
+        return queue1.peek();
+    }
+    
+    public boolean empty() {
+        return queue1.isEmpty() && queue2.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
