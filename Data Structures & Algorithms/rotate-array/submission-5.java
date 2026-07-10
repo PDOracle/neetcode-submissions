219e9ed515e4175706a3db229ed5a103b9821329
Matthew Ange Mao <114012137+PDOracle@users.noreply.github.com>
2026-07-10 17:23:14 -0400

    Add: rotate-array - submission-5

---
+++ Data Structures & Algorithms/rotate-array/submission-5.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int rot = k % nums.length;
+
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, rot - 1);
+        reverse(nums, rot, nums.length - 1);        
+    }
+
+    public void reverse(int[] nums, int l, int r){
+        while(l < r){
+            int tmp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = tmp;
+            l++;
+            r--;
+        }
+    }
+}
