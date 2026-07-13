a1afff23dff4dd9647b7f0759e7e112d2e752829
Matthew Ange Mao <114012137+PDOracle@users.noreply.github.com>
2026-07-13 14:03:52 -0400

    Add: decode-string - submission-10

---
+++ Data Structures & Algorithms/decode-string/submission-10.java
@@ -0,0 +1,57 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<String> stack = new Stack<>();
+        Stack<String> cStack = new Stack<>();
+        StringBuilder res = new StringBuilder();
+        int pointer = 0;
+        String comp = "";
+
+        while(pointer < s.length()){
+            char c = s.charAt(pointer);
+            if(c >= 'a' && c <= 'z'){
+                if(stack.isEmpty()){
+                    res.append(c);
+                }
+                else{
+                    comp += c;
+                }
+                pointer++;
+            }
+            else if(c == '['){
+                //if(!comp.equals("")){
+                    System.out.println("Pointer: " + pointer + ", " + "Comp: " + comp);
+                    cStack.push(comp);
+                    comp = "";
+                //}
+                pointer++;
+            }
+            else if(c == ']'){
+                System.out.println("Pointer: " + pointer + ", " + comp);
+                int loop = Integer.valueOf(stack.pop());
+                String tmp = comp;
+                for(int i = 1; i < loop; i++){
+                    comp += tmp;
+                }
+                if(!cStack.isEmpty()){
+                    comp = cStack.pop() + comp;
+                }
+                if(stack.isEmpty()){
+                    res.append(comp);
+                    comp = "";
+                }
+                System.out.println("Pointer: " + pointer + ", " + "Res: " + res.toString());
+                pointer++;
+            }
+            else if(c > '0' && c <= '9'){
+                String num = "";
+                while(c >= '0' && c <= '9'){
+                    num += c;
+                    pointer++;
+                    c = s.charAt(pointer);
+                }
+                stack.push(num);
+            }
+        }
+        return res.toString();
+    }
+}
